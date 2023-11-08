4490cb466f7e49b5323f308a633db2c7d7a2c921
artursab <arturs.belavskis@gmail.com>
2023-11-08 17:49:48 +0200

    list&queue

---
+++ List/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace List
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            List<int> numbers = new List<int>();
+
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(52);
+
+            numbers.AddRange(new int[] {3, 4, 5, 6});
+
+            numbers.RemoveAt(0);
+
+            numbers.Remove(3);
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+
+
+            Queue<string> patience = new Queue<string>();
+
+            patience.Enqueue("Artis");
+            patience.Enqueue("Nekils");
+            patience.Enqueue("Jasda");
+            patience.Enqueue("Lsaed");
+
+            Console.WriteLine("Now: " + patience.Dequeue());
+            Console.WriteLine("Next is: " + patience.Peek());
+
+            foreach (var patient in patience)
+            {
+                Console.WriteLine(patient);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ Pacman/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Net.Http.Headers;
+
+namespace Pacman
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            char[,] map = ReadMap("map.txt");
+
+        }
+
+        private static char[,] ReadMap(string path)
+        {
+
+            string[] file = File.ReadAllLines("map.txt");
+
+            char[,] map = new char[GetMaxLengthOfLine(file), file.Length];
+
+            for (int x = 0; x < map.GetLength(0); x++)
+                for (int y = 0; y < map.GetLength(1); y++)
+                    map[x, y] = file[y][x];
+
+            return map;
+        }
+        
+        private static int GetMaxLengthOfLine(string[] lines)
+        {
+            int maxLength = lines[0].Length;
+
+            foreach (var line in lines)
+                if(line.Length > maxLength)
+                    maxLength = line.Length;
+
+            return maxLength;
+        } 
+
+
+    }
+}
