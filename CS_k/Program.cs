febabb38d58c7b05a27f46a283025290334c6184
artursab <arturs.belavskis@gmail.com>
2023-11-08 08:36:05 +0200

    fly

--- CS_k/Program.cs
+++ CS_k/Program.cs
@@ -7,27 +7,55 @@ namespace CS_k
     {
         static void Main(string[] args)
         {
-            int[] array = { 2, 3, 4, 5, 6, 7, 8, -2 };
+            //int[] array = { 2, 3, 4, 5, 6, 7, 8, -2 };
 
-            //sum array
-            int sum = 0;
-            for (int i = 0; i < array.Length; i++)
-            {
-                sum += array[i];
-            }
-            Console.WriteLine(sum);
+            ////sum array
+            //int sum = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];
+            //}
+            //Console.WriteLine(sum);
 
-            //max element
-            int MaxElement = int.MinValue;
-            for (int i = 0; i < array.Length; i++)
+            ////max element
+            //int MaxElement = int.MinValue;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    if (MaxElement < array[i])
+            //    {
+            //        MaxElement = array[i];
+            //    }
+            //}
+            //Console.WriteLine(MaxElement);
+            //Console.WriteLine();
+
+
+            //passanger registration for airplane
+            int[] sectors = { 6, 15, 7, 30, 10 };
+
+            while (true)
             {
-                if (MaxElement <  array[i])
+                Console.SetCursorPosition(0, 18);
+                for (int i = 0; i < sectors.Length; i++)
                 {
-                    MaxElement = array[i];
+                    Console.WriteLine($"in sector {i + 1} are free {sectors[i]} seats");
                 }
-            }
-            Console.WriteLine(array);
 
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Registration to fly!");
+
+
+                Console.WriteLine("\n\n1 - book a seat \n2 - exit from program.\n\n");
+                Console.Write("Enter program number: ");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:  
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
 
         }
     }
