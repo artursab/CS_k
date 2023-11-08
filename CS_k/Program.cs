52e86f52182ece2af60ab323379e839c7f4eb055
artursab <arturs.belavskis@gmail.com>
2023-11-08 08:16:25 +0200

    sumarr

--- CS_k/Program.cs
+++ CS_k/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+namespace CS_k
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 5, 6, 7, 8, -2 };
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
