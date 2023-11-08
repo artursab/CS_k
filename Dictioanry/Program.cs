8d38c3cbe55db9f27b789182601755dd6aec1553
artursab <arturs.belavskis@gmail.com>
2023-11-08 18:38:19 +0200

    dictioany

---
+++ Dictioanry/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string, string> countriesCapital = new Dictionary<string, string>();
+
+            countriesCapital.Add("Australia", "Kanberra");
+            countriesCapital.Add("Turkish", "Ankara");
+            countriesCapital.Add("Switz", "Bern");
+
+            //if (countriesCapital.ContainsKey("Australia"))
+            //    Console.WriteLine(countriesCapital["Australia"]);
+
+
+            //foreach (var item in countriesCapital)
+            //{
+            //    Console.WriteLine($"Country - {item.Key} city {item.Value}");
+            //}
+
+            foreach (var key in countriesCapital.Keys)
+            {
+                Console.WriteLine($"Country - {key}");
+            }
+
+        }
+    }
+}
