8adb7d9e5e44fdefe047a7da81b4332654937c57
artursab <arturs.belavskis@gmail.com>
2023-11-08 11:14:55 +0200

    uwu

---
+++ returnFunc/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace write
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 3, y = 4, sum;
+
+            sum = Sum(j,y);
+            Console.WriteLine(sum);
+        }
+
+        static int Sum(int x, int y)
+        {
+            int sum;
+            sum = x + y;
+
+            return sum;
+        }
+
+    }
+}
---
+++ writeerror/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace write
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("HI");
+            Console.WriteLine("HIy");
+            WriteError("Not working");
+            Console.WriteLine("HM");
+            WriteError("Go pay");
+        }
+
+        static void WriteError(string text)
+        {
+            ConsoleColor consoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ForegroundColor = consoleColor;
+        }
+
+    }
+}
